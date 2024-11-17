3c0a2af1c3f405c1f1739a746620672664bd9c55
AkanchaBanka <akancha.banka8@gmail.com>
2024-11-17 08:35:23 -0800

    igniting my app with parcel

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement("div", { id: "parent"},[
     React.createElement("div",{ id: "child1" },[
         React.createElement("h1",{},"I am an h1 tag"),
         React.createElement("h2",{},"I am an h2 tag"),
     ]),
     React.createElement("div",{ id: "child2" },[
-        React.createElement("h1",{},"I am an h1 tag"),
+        React.createElement("h1",{},"ghfjhkglu"),
         React.createElement("h2",{},"I am an h2 tag"),
     ])
 ]);
