380a4e11268d441a6432871bbdf02d7dd5d009e4
AkanchaBanka <akancha.banka8@gmail.com>
2025-02-22 20:24:25 -0800

    added build script

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,10 +11,6 @@ const Body = () => {
     const [searchText,setSearchText] = useState("");
     const [listOfRestaurant, setListOfRestaurant] = useState([]);
 
-    useEffect(() => {
-        fetchList();
-    },[])
-
     const fetchList = async() => {
         const data = await fetch(
             "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
@@ -24,6 +20,10 @@ const Body = () => {
         setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
         setFilteredListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
     }
+
+    useEffect(() => {
+        fetchList();
+    },[])
 
     const onlineStatus = useOnlineStatus();
     console.log(onlineStatus);
