a4a7fbe0bf59d53376147401e37f31b8f93df8a2
AkanchaBanka <akancha.banka8@gmail.com>
2025-02-24 17:48:31 -0800

    adding User-Agent header

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const axios = require('axios');
 
 const app = express();
 
+const headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+};
+
 // Middleware to handle CORS and JSON responses
 app.use(express.json());
 app.use((req, res, next) => {
@@ -18,7 +22,7 @@ app.get("/", async (req, res) => {
     const apiUrl = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING';
 
     try {
-        const response = await axios.get(apiUrl);
+        const response = await axios.get(apiUrl, {headers});
         // Directly use response.data since Axios handles JSON parsing
         console.log(response.data);
         res.status(200).json(response.data);
